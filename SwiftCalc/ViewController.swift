bcc6f88da45da24d26d2bc18682fe0e460c30ea0
Stephen Maitland <s.maitland@me.com>
2015-05-01 18:51:56 +0100

    Initial Commit

---
+++ SwiftCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCalc
+//
+//  Created by Stephen on 01/05/2015.
+//  Copyright (c) 2015 vantech.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
